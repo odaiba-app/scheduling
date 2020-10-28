fa16860dbc4cc2951a985484834966d9fdcfbb3b
Liam Baker <liam.baker2310@gmail.com>
2020-10-28 16:37:37 +0900

    connected react to views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,6 +26,9 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
+
+import "../schedule";
+
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
