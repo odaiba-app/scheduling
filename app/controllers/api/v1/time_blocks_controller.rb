f2f7d44bf8fe9a58a9639d6928a5eda8bde7d376
Liam Baker <liam.baker2310@gmail.com>
2020-10-30 19:25:26 +0900

    time should be formatted for users around the world

--- app/controllers/api/v1/time_blocks_controller.rb
+++ app/controllers/api/v1/time_blocks_controller.rb
@@ -2,36 +2,36 @@ class Api::V1::TimeBlocksController < Api::V1::BaseController
   before_action :configure_user_time, except: :show
 
   def monday
-    @timeblocks = FindDay.call(@blocks, 'monday')
+    @timeblocks = FindDay.call(@blocks, 'monday').drop(12)
     render :friday
   end
 
   def tuesday
-    @timeblocks = FindDay.call(@blocks, 'tuesday')
+    @timeblocks = FindDay.call(@blocks, 'tuesday').drop(12)
     render :friday
   end
 
   def wednesday
-    @timeblocks = FindDay.call(@blocks, 'wednesday')
+    @timeblocks = FindDay.call(@blocks, 'wednesday').drop(12)
     render :friday
   end
 
   def thursday
-    @timeblocks = FindDay.call(@blocks, 'thursday')
+    @timeblocks = FindDay.call(@blocks, 'thursday').drop(12)
     render :friday
   end
 
   def friday
-    @timeblocks = FindDay.call(@blocks, 'friday')
+    @timeblocks = FindDay.call(@blocks, 'friday').drop(12)
   end
 
   def saturday
-    @timeblocks = FindDay.call(@blocks, 'saturday')
+    @timeblocks = FindDay.call(@blocks, 'saturday').drop(12)
     render :friday
   end
 
   def sunday
-    @timeblocks = FindDay.call(@blocks, 'sunday')
+    @timeblocks = FindDay.call(@blocks, 'sunday').drop(12)
     render :friday
   end
 
