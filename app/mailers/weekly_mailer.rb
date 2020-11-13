f8e2302490d3ec21f1ca76213d19d392187d0ad5
Paulo DAlberti <phdalberti@gmail.com>
2020-11-13 20:47:15 +0900

    mailers

--- app/mailers/weekly_mailer.rb
+++ app/mailers/weekly_mailer.rb
@@ -5,10 +5,12 @@ class WeeklyMailer < ApplicationMailer
   end
 
   def remind_to_input
-
+    @user = params[:user]
+    mail(to: @user.email, subject: '[Scheduler] Please input your availability')
   end
 
   def prompt_to_collaborate
-
+    @user = params[:user]
+    mail(to: @user.email, subject: '[Scheduler] Get ready to collaborate!')
   end
 end
