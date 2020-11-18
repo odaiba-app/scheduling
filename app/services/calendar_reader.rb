9c9ad9f2e428adb82139cbcc109ba89f0b36f7c3
Paulo DAlberti <phdalberti@gmail.com>
2020-11-18 22:52:22 +0900

    calendar reader and test script

---
+++ app/services/calendar_reader.rb
@@ -0,0 +1,38 @@
+require 'open-uri'
+
+class CalendarReader
+  def initialize(calendar_links)
+    @calendar_links = calendar_links
+    @events =         []
+    @time_slots =     []
+    @start_time =     TimeBlock.first.time
+    @end_time =       TimeBlock.last.time
+  end
+
+  def next_week_events
+    read_events
+    break_into_30_minutes_slots
+    @time_slots.sort
+  end
+
+  private
+
+  def read_events
+    @calendar_links.each do |calendar|
+      cal = Icalendar::Calendar.parse(URI.open(calendar).read).first
+      cal.events.each do |event|
+        @events << event if event.dtstart > @start_time && event.dtstart < @end_time
+      end
+    end
+  end
+
+  def break_into_30_minutes_slots
+    @events.each do |event|
+      start_time = event.dtstart
+      until start_time >= event.dtend
+        @time_slots << start_time unless @time_slots.include?(start_time)
+        start_time += 30.minutes
+      end
+    end
+  end
+end
---
+++ script/temp/20201118_test_calendar_reader.rb
@@ -0,0 +1,22 @@
+puts 'Select index of user with calendars'
+
+user_ids = Calendar.all.pluck(:user_id).uniq
+users = User.where(id: user_ids)
+
+users.each_with_index do |user, idx|
+  puts "index #{idx} for user #{user.username}"
+end
+print '>'
+index = gets.chomp.to_i
+
+user = users[index]
+
+puts "Creating time blocks for user #{user.username}"
+
+events = CalendarReader.new(user.calendars.pluck(:link)).next_week_events
+time_blocks = TimeBlock.where(time: events)
+time_blocks.each do |time_block|
+  UserAvailability.create!(user: user, time_block: time_block)
+end
+
+puts 'finished'
