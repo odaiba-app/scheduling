91bf93ba291411014d84f8201fc3ca42221e09da
Roomee <sungmee0219@gmail.com>
2020-11-18 21:34:35 -0800

    flashes email notification changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		@user.weekly_email = !@user.weekly_email 
 		@user.save
-		redirect_to user_root_path
+		# redirect_to user_root_path
+		respond_to do |format|
+			format.js {flash.now[:notice] = "Email Notification Changed Successfully"}
+		end
 	end
 end
