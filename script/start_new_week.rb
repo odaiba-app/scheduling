5abf5d0784c1c305748f11b25dc443d7e57d2eca
Liam Baker <liam.baker2310@gmail.com>
2020-11-22 17:39:20 +0900

    fixed script

--- script/start_new_week.rb
+++ script/start_new_week.rb
@@ -1,6 +1,6 @@
 UserAvailability.destroy_all
 TimeBlock.update_all("time = time + interval '7' day")
 Company.first.update!(reminder_sent: false)
-User.where(weekly_email: true).each do |user| 
+User.where(email_notification_enabled: true).each do |user|
 	WeeklyMailer.with(user: user).start_new_week.deliver_now
 end
