b658e57a4e7afa0132a6610c5603cd57552e03ec
Paulo DAlberti <phdalberti@gmail.com>
2020-11-23 22:31:11 +0900

    activate user

---
+++ db/migrate/20201123125722_add_active_column_to_user.rb
@@ -0,0 +1,5 @@
+class AddActiveColumnToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :active, :boolean, default: true
+  end
+end
--- script/start_new_week.rb
+++ script/start_new_week.rb
@@ -1,3 +1,5 @@
+active_users_ids = UserAvailability.all.pluck(:user_id).uniq
+User.where.not(id: active_users_ids).update_all(active: false)
 UserAvailability.destroy_all
 TimeBlock.update_all("time = time + interval '7' day")
 Company.first.update!(reminder_sent: false)
