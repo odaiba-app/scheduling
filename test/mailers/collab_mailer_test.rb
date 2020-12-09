c4727f06b588c549996db6fc570ca999e6549f2c
Paulo DAlberti <phdalberti@gmail.com>
2020-12-09 21:45:49 +0900

    collab mailer

---
+++ test/mailers/collab_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CollabMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/mailers/previews/collab_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/collab_mailer
+class CollabMailerPreview < ActionMailer::Preview
+
+end
