c8d028b30af7c8920d185e109f8a268f5136c901
Paulo DAlberti <phdalberti@gmail.com>
2020-12-21 18:12:13 +0900

    BE implementation

--- app/controllers/api/v1/time_blocks_controller.rb
+++ app/controllers/api/v1/time_blocks_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::TimeBlocksController < Api::V1::BaseController
-  before_action :configure_user_time, except: :show
+  before_action :configure_user_time, except: %i[show invite_to_collab]
+  before_action :set_time_block, only: %i[remove_user_availability invite_to_collab]
 
   def day
     @time_blocks = FindDay.call(@blocks, params[:day]).take(48).drop(12)
@@ -10,15 +11,35 @@ class Api::V1::TimeBlocksController < Api::V1::BaseController
   end
 
   def remove_user_availability
-    time_block = TimeBlock.find(params[:id])
     user_availability = time_block.user_availabilities.where(user: current_user).first
     user_availability.destroy
     render json: time_block
+  end
+
+  def invite_to_collab
+    user = User.find(params[:user_id])
+    skills = SharedSkillsMatcher.new(user, current_user).call
+    raise StandardError.new('No shared skills') if skills.blank?
+
+    to_user_time = time_block.time.in_time_zone(user.time_zone)
+    CollabMailer.with(
+      from_user: current_user,
+      to_user: user,
+      skills: skills,
+      to_user_time: to_user_time
+    ).invite_to_collaborate.deliver_now
+    render json: { message: "Invitation to #{user.username} sent" }, status: 200
+  rescue StandardError => e
+    render json: { error: "#{e}" }, status: 422
   end
 
   private
 
   def configure_user_time
     @blocks = TimeBlock.includes(:user_availabilities).configure_time(current_user)
+  end
+
+  def set_time_block
+    time_block = TimeBlock.find(params[:id])
   end
 end
--- app/mailers/collab_mailer.rb
+++ app/mailers/collab_mailer.rb
@@ -3,6 +3,7 @@ class CollabMailer < ApplicationMailer
     @from_user = params[:from_user]
     @to_user = params[:to_user]
     @skills = params[:skills]
+    @to_user_time = params[:to_user_time]
     mail(to: @to_user.email, subject: "[Scheduler] #{@from_user.username} would like to collaborate")
   end
 end
